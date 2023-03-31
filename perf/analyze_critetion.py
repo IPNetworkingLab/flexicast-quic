ba1e550cb337359f4ee90f3c1e4aec99ef6be09a
Louis Navarre <louisnavarre@hotmail.com>
2023-03-31 11:44:19 +0000

    Add benchmark parsing result

---
+++ perf/analyze_critetion.py
@@ -0,0 +1,68 @@
+import matplotlib.pyplot as plt
+import os
+import json
+import numpy as np
+
+
+def parse_json(filename):
+    with open(filename) as fd:
+        data = json.load(fd)
+    return np.median(data["times"]) / 1000000000, np.std(data["times"]) / 1000000000
+
+
+def read_unicast(dirname):
+    all_data = dict()
+    for subdir in os.listdir(dirname):
+        file_path = os.path.join(dirname, subdir, "new", "sample.json")
+        all_data[int(subdir)] = (parse_json(file_path))
+    return all_data
+
+
+def read_multicast(dirname):
+    auth_asym = dict()
+    no_auth = dict()
+    auth_sym = dict()
+    
+    for subdir in os.listdir(dirname):
+        filepath = os.path.join(dirname, subdir, "new", "sample.json")
+        data = parse_json(filepath)
+
+        auth_tag = subdir.split("-")[0]
+        nb_recv = int(subdir.split("-")[1])
+        if auth_tag == "AsymSign":
+            auth_asym[nb_recv] = data
+        elif auth_tag == "SymSign":
+            auth_sym[nb_recv] = data
+        elif auth_tag == "None":
+            no_auth[nb_recv] = data
+    
+    return auth_asym, auth_sym, no_auth
+
+
+def cmp_mc_uc(root):
+    mc_auth_asym, mc_auth_sym, mc_no_auth = read_multicast(os.path.join(root, "multicast-1G"))
+    uc = read_unicast(os.path.join(root, "unicast-1G"))
+
+    data = [
+        (uc, "Unicast"),
+        (mc_auth_asym, "Multicast asymetric"),
+        (mc_auth_sym, "Multicast symetric"),
+        (mc_no_auth, "Multicast no authentication")
+    ]
+
+    fig, ax = plt.subplots()
+    for d, label in data:
+        k = sorted(d.keys())
+        v = [d[i][0] for i in k]
+        std = [d[i][1] for i in k]
+
+        ax.errorbar(k, v, yerr=std, label=label, fmt="-o")
+
+    ax.legend()
+    ax.set_xlabel("Number of receivers")
+    ax.set_ylabel("Time to send 1GB [s]")
+
+    plt.savefig("test.pdf")
+
+if __name__ == "__main__":
+    cmp_mc_uc("../target/criterion")
