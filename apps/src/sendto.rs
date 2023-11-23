676bc7888045bd9005eaf0ce8c7f0b8794dd1bf1
Louis Navarre <louisnavarre@hotmail.com>
2023-11-23 11:15:20 +0000

    Revert change in sendto.rs

--- apps/src/sendto.rs
+++ apps/src/sendto.rs
@@ -35,7 +35,11 @@ pub fn detect_gso(socket: &mio::net::UdpSocket, segment_size: usize) -> bool {
     use nix::sys::socket::sockopt::UdpGsoSegment;
     use std::os::unix::io::AsRawFd;
 
-    setsockopt(socket.as_raw_fd(), UdpGsoSegment, &(segment_size as i32)).is_ok()
+    // setsockopt(socket.as_raw_fd(), UdpGsoSegment, &(segment_size as i32)).is_ok()
+    let fd = unsafe { std::os::fd::BorrowedFd::borrow_raw(socket.as_raw_fd()) };
+
+    setsockopt(&fd, UdpGsoSegment, &(segment_size as i32)).is_ok()
+
 }
 
 /// For non-Linux, there is no GSO support.
