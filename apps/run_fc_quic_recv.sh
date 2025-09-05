953bd3e65611bacdfe44febbf7a94c76487464d1
Louis Navarre <louisnavarre@hotmail.com>
2025-09-05 11:37:30 +0200

    Update run fcquic recv file with new app

--- apps/run_fc_quic_recv.sh
+++ apps/run_fc_quic_recv.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-if [ $APP_FILE -eq 1 ] ; then
-    APP_ARGS=--stay-open
-else
-    APP_ARGS="--video /shared"
-fi
+check_app() {
+    case $1 in
+        rss )
+            echo "RSS application"
+            APP_ARGS="--stay-open --transfer-kind file,file,/shared"
+            ;;
+        
+        bbb )
+            echo "Big Buck Bunny application"
+            APP_ARGS="--transfer-kind stream,hls,/shared"
+            ;;
+        
+        ssim )
+            echo "SSIM application"
+            APP_ARGS="--transfer-kind stream,rtp,127.0.0.1:22222"
+            ;;
 
-RUST_LOG=trace fc-recv-file-transfer -l 0.0.0.0 https://$FC_SRC_IP:$FC_SRC_PORT/data.txt --output-prefix /shared --flow-control $FC_FLOWCONTROL $FLEXICAST $APP_ARGS
+        *)
+            echo "Unsupported application"
+            exit 1
+            ;;
+    esac
+}
+
+check_app $APP_KIND
+
+RUST_LOG=trace fc-recv-file-transfer -l 0.0.0.0 https://$FC_SRC_IP:$FC_SRC_PORT/data.txt --flow-control $FC_FLOWCONTROL $FLEXICAST $APP_ARGS
